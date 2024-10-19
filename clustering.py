c6b617eb2884a51715eb0b2f6fa9c2107f8f2990
Christ Chadrak MVOUNGOU <chadrakchristmvoungou@gmail.com>
2024-10-19 17:15:44 +0200

    Create data collection function. It allow us to get points and store it in the dictionary.

--- clustering.py
+++ clustering.py
@@ -1,1 +1,16 @@
-print("En tout cas, rien ne se passe ici ")
+import numpy as np
+from math import sqrt
+
+"""
+@param : numbers_point -> This param allow user to set the number of point at the start
+This function allow the collection of points
+And store the data inside of dictionary
+"""
+def data_collection(numbers_points):
+  coordinates = []
+  for i in range(numbers_points):
+    x = float(input(f"Enter the abscis of point {i+1} : "))
+    y = float(input(f"Enter the ordinate of point {i+1} : "))
+    coordinates.append({"x" : x, "y": y})
+  return coordinates
+
