f84f7c451605b593ea6005a34a0645639d056caa
Christ Chadrak MVOUNGOU <chadrakchristmvoungou@gmail.com>
2024-10-19 17:30:15 +0200

    One we have the data in two lists, we can normalized it now. Using numpy function for mean and std and also by using the formula

--- clustering.py
+++ clustering.py
@@ -31,3 +31,20 @@ def extract_data(data):
         coord_x_list.append(point["x"])
         coord_y_list.append(point["y"])
     return coord_x_list, coord_y_list
+
+"""
+@params : list_coord_x and list_coord_y
+We get the data in separate list from the function extract data and now, we can do some preprocessing
+We prepare our data because we want to avoid the scale effects in our calculus.
+"""
+
+def data_normalization(list_coord_x, list_coord_y):
+    # We transform the list to a array numpy because we have more flexibity when we work with numpy. We have more function ready to help us
+    list_coord_x_array = np.array(list_coord_x)
+    list_coord_y_array = np.array(list_coord_y)
+
+    # With the formula of normalization 
+    liste_coord_x_normalized = (list_coord_x_array - np.mean(list_coord_x_array)) / np.std(list_coord_x_array)
+    liste_coord_y_normalized = (list_coord_y_array - np.mean(list_coord_y_array)) / np.std(list_coord_y_array)
+
+    return liste_coord_x_normalized, liste_coord_y_normalized
